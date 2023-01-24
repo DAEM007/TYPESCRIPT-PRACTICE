2fa1c40b8ebbff2444c813424031ab0d634e526d
EMMANUEL <dammylola299@gmail.com>
2023-01-24 11:44:34 +0100

    added topics to cover

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,17 @@
 "use strict";
+// Types
 let id = 5;
 console.log(`ID: ${id}`);
+// Arrays and tuples
+// object types
+// Enums
+// Aliases and intefaces
+// Union Types
+// Functions
+// Casting
+// Classes
+// Basic Generics
+// Utility types
+// Keyof
+// Null
+// Definitely typed
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,30 @@
-let id = 5;
 
+// Types
+let id = 5;
 console.log(`ID: ${id}`);
+
+// Arrays and tuples
+
+// object types
+
+// Enums
+
+// Aliases and intefaces
+
+// Union Types
+
+// Functions
+
+// Casting
+
+// Classes
+
+// Basic Generics
+
+// Utility types
+
+// Keyof
+
+// Null
+
+// Definitely typed
